64ee1f6c520784f4256944b21fd78b2433d35347
joel <jzambranojazc2001gmail.com>
2024-08-07 19:34:15 -0400

    fix responsive

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,15 +5,10 @@ import projectImg from '@/../public/assets/projectImg.png';
 import {
   AddButton,
   Header,
-  List,
-  ListItem,
   ProjectsCards,
-  Select,
-  SideBar,
   TaskFilter,
   TasksCards,
   Text,
-  TextBox,
   Title,
   WorkSpaces,
 } from '@/app/dashboard/components/Components';
@@ -22,6 +17,7 @@ import Icon from '@/components/UI/Icon';
 import { Box, Switch } from '@mui/material';
 import Image from 'next/image';
 import { useState } from 'react';
+import SideBarComponent from './components/SideBar/SideBar';
 
 const Dashboard = () => {
   const [TaskInfo, setTaskInfo] = useState([
@@ -67,55 +63,28 @@ const Dashboard = () => {
           description: 'Lorem Ipsum Dolor',
           status: false,
         },
+        {
+          description: 'Lorem Ipsum Dolor',
+          status: false,
+        },
+        {
+          description: 'Lorem Ipsum Dolor',
+          status: true,
+        },
+        {
+          description: 'Lorem Ipsum Dolor',
+          status: true,
+        },
+        {
+          description: 'Lorem Ipsum Dolor',
+          status: false,
+        },
       ],
     },
   ]);
-  const NavList = [
-    'Dashboard',
-    'Tareas',
-    'Proyectos',
-    'Equipo',
-    'Finanzas',
-    'Contactos',
-    'Ajustes',
-  ];
+
 
   const UserName = 'Pepito Perez';
-
-  //handle if active or not for list items
-  const handleActive = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    // on click change the active class
-    const target = event.target as HTMLDivElement;
-    target.classList.toggle('active');
-
-    // remove the active class from the siblings
-    const siblings = target.parentElement?.children;
-    for (let i = 0; i < siblings!.length; i++) {
-      if (siblings![i] !== target) {
-        siblings![i].classList.remove('active');
-      }
-    }
-
-    //change the icon color if active
-    const icon = target.querySelector('img');
-    if (target.classList.contains('active')) {
-      icon!.style.filter = 'invert(100%)';
-    } else {
-      icon!.style.filter =
-        'invert(0%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%)';
-    }
-
-    //remove the active class from the siblings icons
-    for (let i = 0; i < siblings!.length; i++) {
-      const siblingIcon = siblings![i].querySelector('img');
-      if (siblings![i] !== target) {
-        siblingIcon!.style.filter =
-          'invert(0%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%)';
-      }
-    }
-  };
 
   return (
     <Box
@@ -125,63 +94,28 @@ const Dashboard = () => {
         backgroundColor: '#FCF6F1',
       }}
     >
-      <SideBar>
-        <Container gap={{ xs: 1, lg: 3, xl: 5 }}>
-          <Item xs={12}>
-            <Title textAlign={'left'}>Workit</Title>
-          </Item>
-          <Item xs={12}>
-            <Container>
-              <Select>
-                <option>Intelligent AI</option>
-                <option>Proyectos activos</option>
-                <option>Proyectos completados</option>
-              </Select>
-            </Container>
-          </Item>
-
-          <Item
-            sx={{
-              height: '60%',
-            }}
-          >
-            <List>
-              {NavList.map((item, index) => (
-                //handle with a class if active or not
-                <ListItem key={index} onClick={handleActive}>
-                  <Icon src='home' size={15} />
-                  {item}
-                </ListItem>
-              ))}
-            </List>
-          </Item>
-          <Item sx={{ marginTop: 10 }}>
-            <TextBox>
-              <Box display={'flex'} flexDirection={'row'} gap={1} width={116}>
-                <Icon src='home' size={15} />
-                Dark Mode
-              </Box>
-              <Switch />
-            </TextBox>
-          </Item>
-        </Container>
-      </SideBar>
+      <SideBarComponent />
       <Box>
         <Header>
-          <Box display={'flex'} flexDirection={'row'} gap={4}>
-            <Image src={profileImg} alt='profile pic' width={56} height={56} />
-            <Title textAlign={'left'}>Hola, {UserName}</Title>
+          <Box display={'flex'} alignItems={"center"} flexDirection={'row'} gap={4}>
+            <Box sx={{ width: { md: 36, xl: 48 }, height: { md: 36, xl: 48 }, position: 'relative' }}>
+              <Image src={profileImg} fill objectFit='contain' alt='profile pic' />
+            </Box>
+
+            <Title sx={{ fontSize: { md: 24, xl: 36 } }} textAlign={'left'}>Hola, {UserName}</Title>
           </Box>
           <Box display={'flex'} flexDirection={'row'} gap={4}>
-            <Icon src='search' size={56} />
-            <Icon src='notification' size={56} />
+            <Icon src='search' size={36} />
+            <Icon src='notification' size={36} />
           </Box>
         </Header>
 
-        <WorkSpaces>
+        <WorkSpaces sx={{
+          padding: { md: 2, xl: 4 }
+        }}>
           <Container>
             <Item xs={9}>
-              <Text size={32}>Tareas</Text>
+              <Text sx={{ fontSize: { md: 24, xl: 28 } }} >Tareas</Text>
               <TaskFilter>
                 <Text
                   size={14}
@@ -229,31 +163,31 @@ const Dashboard = () => {
             <Item xs={3}>
               <AddButton>
                 Agregar
-                <Icon src='AddIcon' size={32} />
+                <Icon src='AddIcon' size={24} />
               </AddButton>
             </Item>
 
-            <Item sx={{ marginTop: 2 }}>
-              <Box sx={{ display: 'flex', flexDirection: 'row', gap: 3 }}>
+            <Item sx={{ marginTop: 1 }}>
+              <Box sx={{ display: 'flex', flexDirection: 'row', gap: { md: 1, xl: 3 } }}>
                 {TaskInfo.length > 0
                   ? TaskInfo.map((task, index) => (
-                      <TasksCards
-                        setTaskInfo={setTaskInfo}
-                        key={index}
-                        date={task.date}
-                        taskInfo={TaskInfo}
-                        tasks={task.tasks}
-                        cardStatus={
-                          task.tasks
-                            .filter((task) => task.status)
-                            .length.toString() +
-                          '/' +
-                          task.tasks.length.toString()
-                        }
-                        colors={task.color}
-                        projectImg={projectImg}
-                      />
-                    ))
+                    <TasksCards
+                      setTaskInfo={setTaskInfo}
+                      key={index}
+                      date={task.date}
+                      taskInfo={TaskInfo}
+                      tasks={task.tasks}
+                      cardStatus={
+                        task.tasks
+                          .filter((task) => task.status)
+                          .length.toString() +
+                        '/' +
+                        task.tasks.length.toString()
+                      }
+                      colors={task.color}
+                      projectImg={projectImg}
+                    />
+                  ))
                   : null}
               </Box>
             </Item>
@@ -261,12 +195,12 @@ const Dashboard = () => {
 
           <Container>
             <Item xs={9}>
-              <Text size={32}>Proyectos</Text>
+              <Text sx={{ fontSize: { md: 24, xl: 28 } }} >Proyectos</Text>
             </Item>
             <Item xs={3}>
               <AddButton>
                 Agregar
-                <Icon src='AddIcon' size={32} />
+                <Icon src='AddIcon' size={24} />
               </AddButton>
             </Item>
 
