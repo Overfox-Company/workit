9303f837769f5b6d1b469260abf690ccc78f8849
overfox-company <overfoxcompany@gmail.com>
2024-11-15 16:47:31 -0400

    update avatar component

--- src/app/dashboard/components/DashBoardLayout/components/AvatarComponent.tsx
+++ src/app/dashboard/components/DashBoardLayout/components/AvatarComponent.tsx
@@ -1,53 +1,78 @@
+'use client'
 import { PRIMARYDARK } from '@/constants/Colors'
 import { AuthContext } from '@/context/AuthContext'
 import { Avatar } from '@mui/material'
 import { NextPage } from 'next'
 import Image from 'next/image'
 import { useContext, useState } from 'react'
-
+import { motion } from 'framer-motion'
+import styled from '@emotion/styled';
 interface Props { }
+const Container = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+`;
 
-const AvatarComponent: NextPage<Props> = ({ }) => {
-    const [openProfileOption, setOpenProfileOptions] = useState(false)
-    const { user } = useContext(AuthContext)
+const RoundedContainer = styled.div`
+  cursor: pointer;
+  border-radius: 200px;
+  background-color: rgba(130, 140, 160, 0.3);
+  padding: 4px;
+  gap: 2px;
+  display: flex;
+  align-items: center;
+`;
+
+const RelativeWrapper = styled.div`
+  position: relative;
+`;
+
+const StatusIndicator = styled.div`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  height: 10px;
+  width: 10px;
+  border-radius: 200px;
+  padding: 1px;
+  border: solid 1px ${PRIMARYDARK};
+  background-color: #2fff51;
+`;
+
+const AvatarComponent: NextPage<Props> = () => {
+    const [openProfileOption, setOpenProfileOptions] = useState(false);
+    const { user } = useContext(AuthContext);
+
+    const handleToggle = () => {
+        setOpenProfileOptions(!openProfileOption);
+    };
+
     return (
-        <div style={{ display: 'flex', gap: 8, alignItems: 'center' }}>
-
-            <div style={{
-                cursor: 'pointer',
-                borderRadius: 200,
-                backgroundColor: 'rgba(130,140,160,0.3)',
-                padding: 4,
-                gap: 4,
-                display: 'flex',
-                alignItems: 'center',
-            }}>
-                <div style={{ position: 'relative' }}>
+        <Container>
+            <RoundedContainer onClick={handleToggle}>
+                <RelativeWrapper>
                     <Avatar
                         src={user.avatar ? user.avatar : undefined}
-                        alt='profile pic'
+                        alt="profile pic"
                         sx={{ width: 28, height: 28 }}
-                    >{
-                            user.avatar ? null : user.name?.split("")[0]}</Avatar>
-                    <div style={{
-                        position: 'absolute',
-                        right: 0,
-                        bottom: 0,
-                        height: 10,
-                        width: 10,
-                        borderRadius: 200,
-                        padding: 1,
-                        border: `solid 1px ${PRIMARYDARK}`,
-                        backgroundColor: '#2FFF51'
-                    }} />
-                </div>
-
-                <Image src={'/assets/arrowfilters.svg'} width={16} height={16} alt="" />
-            </div>
-
-        </div>
-    )
-
-}
-
+                    >
+                        {user.avatar ? null : user.name?.split('')[0]}
+                    </Avatar>
+                    <StatusIndicator />
+                </RelativeWrapper>
+                <motion.div
+                    animate={{
+                        rotate: openProfileOption ? 180 : 0,
+                    }}
+                    transition={{
+                        duration: 0.3,
+                    }}
+                >
+                    <Image src={'/assets/arrowfilters.svg'} width={16} height={16} alt="" />
+                </motion.div>
+            </RoundedContainer>
+        </Container>
+    );
+};
 export default AvatarComponent
