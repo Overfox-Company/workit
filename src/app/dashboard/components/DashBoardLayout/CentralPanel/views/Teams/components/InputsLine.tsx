a3f5584e6436a788f313e2e9d5089a52d2a1a598
overfox-company <overfoxcompany@gmail.com>
2024-12-30 13:27:13 -0400

    adjust inputs widht

--- src/app/dashboard/components/DashBoardLayout/CentralPanel/views/Teams/components/InputsLine.tsx
+++ src/app/dashboard/components/DashBoardLayout/CentralPanel/views/Teams/components/InputsLine.tsx
@@ -1,71 +1,84 @@
+import { Container, Item } from '@/components/layout/Container';
 import { ButtonBlue, ButtonBlueOutlined } from '@/components/UI/Buttons';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import SearchIcon from '@mui/icons-material/Search';
 import { Box, InputAdornment, TextField } from '@mui/material';
 import { NextPage } from 'next';
 
-interface Props {}
-export const InputsLine: NextPage<Props> = ({}) => {
+interface Props { }
+export const InputsLine: NextPage<Props> = ({ }) => {
   return (
     <Box
       sx={{
         display: 'flex',
+        justifyContent: 'space-between',
         flexDirection: 'row',
         gap: 3,
         marginTop: 2,
       }}
     >
-      <TextField
-        hiddenLabel
-        id='outlined-size-small'
-        size='small'
-        placeholder='Buscar por nombre o correo'
-        sx={{
-          width: '265px',
-          '& .MuiInputBase-root': {
-            height: 48,
-          },
-        }}
-        slotProps={{
-          input: {
-            startAdornment: (
-              <InputAdornment position='start'>
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          },
-        }}
-      />
-      <TextField
-        hiddenLabel
-        id='outlined-size-small'
-        size='small'
-        placeholder='Invitar por email'
-        sx={{
-          width: '397px',
-          '& .MuiInputBase-root': {
-            height: 48,
-          },
-        }}
-        slotProps={{
-          input: {
-            startAdornment: (
-              <InputAdornment position='start'>
-                <MailOutlineIcon />
-              </InputAdornment>
-            ),
-          },
-        }}
-      />
-      <ButtonBlue
-        style={{ width: '140px' }}
-        onClick={() => console.log('click')}
-      >
-        Invitar
-      </ButtonBlue>
-      <ButtonBlueOutlined style={{ width: '133px' }}>
-        Subir CSV
-      </ButtonBlueOutlined>
+      <Container alignItems='center' columnSpacing={2}>
+        <Item xs={9} sx={{ display: 'flex', gap: 3 }}>
+          <TextField
+            hiddenLabel
+            id='outlined-size-small'
+            size='small'
+            placeholder='Buscar por nombre o correo'
+            sx={{
+              width: '265px',
+              '& .MuiInputBase-root': {
+                height: 48,
+              },
+            }}
+            slotProps={{
+              input: {
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              },
+            }}
+          />
+          <TextField
+            hiddenLabel
+            id='outlined-size-small'
+            size='small'
+            placeholder='Invitar por email'
+            sx={{
+              width: '100%',
+              '& .MuiInputBase-root': {
+                height: 48,
+              },
+            }}
+            slotProps={{
+              input: {
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <MailOutlineIcon />
+                  </InputAdornment>
+                ),
+              },
+            }}
+          />
+        </Item>
+        <Item xs={3}>
+          <Box sx={{ gap: 2, display: "flex", justifyContent: 'flex-end' }}>
+            <ButtonBlue
+
+              onClick={() => console.log('click')}
+            >
+              Invitar
+            </ButtonBlue>
+            <ButtonBlueOutlined >
+              Subir CSV
+            </ButtonBlueOutlined>
+          </Box>
+
+        </Item>
+      </Container>
+
+
     </Box>
   );
 };
